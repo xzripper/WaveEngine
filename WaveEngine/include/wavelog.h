f7ba4fc453115484073a037bac0c7315847d7d6f
Ivan Perzhinsky <94743980+xzripper@users.noreply.github.com>
2023-06-11 16:53:24 +0300

    WaveLog update.
    WaveLog update.

--- WaveEngine/include/wavelog.h
+++ WaveEngine/include/wavelog.h
@@ -1,17 +1,28 @@
 #include <iostream>
 
-// Use StringFormatter and make more flexible.
+#include <vector>
+
+#include <cstring>
+
+#include "utils/strformat.h"
+
+#include "utils/strcaseconv.h"
+
 class WaveLog {
 public:
-    static void info(const std::string& text) { std::cout << "WaveLog INFO: " << text << std::endl; }
-    static void info(const std::string& text, const std::string& from) { std::cout << "WaveLog INFO: " << (text + " ") << "[" << from << "];" << std::endl; }
+    static void log(const std::string &level, const std::string &text) { std::cout << StringFormat::format("WaveLog %1: %2\n", std::vector<std::string> {StringCaseConverter::toUpper(level), text}); }
 
-    static void warning(const std::string& text) { std::cout << "WaveLog WARNING: " << text << std::endl; }
-    static void warning(const std::string& text, const std::string& from) { std::cout << "WaveLog WARNING: " << (text + " ") << "[" << from << "];" << std::endl; }
+    static void log(const std::string &level, const std::string &text, const std::string &from) { std::cout << StringFormat::format("WaveLog %1: %2 [%3];\n", std::vector<std::string> {StringCaseConverter::toUpper(level), text, from}); }
 
-    static void error(const std::string& text) { std::cout << "WaveLog ERROR: " << text << std::endl; }
-    static void error(const std::string& text, const std::string& from) { std::cout << "WaveLog ERROR: " << (text + " ") << "[" << from << "];" << std::endl; }
+    static void info(const std::string& text) { log("info", text); }
+    static void info(const std::string& text, const std::string& from) { log("info", text, from); }
 
-    static void critical(const std::string& text) { std::cout << "WaveLog CRITICAL: " << text << std::endl; }
-    static void critical(const std::string& text, const std::string& from) { std::cout << "WaveLog CRITICAL: " << (text + " ") << "[" << from << "];" << std::endl; }
+    static void warning(const std::string& text) { log("warning", text); }
+    static void warning(const std::string& text, const std::string& from) { log("warning", text, from); }
+
+    static void error(const std::string& text) { log("error", text); }
+    static void error(const std::string& text, const std::string& from) { log("error", text, from); }
+
+    static void critical(const std::string& text) { log("critical", text); }
+    static void critical(const std::string& text, const std::string& from) { log("critical", text, from); }
 };
